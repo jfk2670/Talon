2d6dfeec902f3ffd65e2db221caf027c153fabfc
root <root@localhost.localdomain>
2016-04-30 17:41:22 -0400

    Added logger object, fixed date search

---
+++ README.md
@@ -0,0 +1,36 @@
+## Synopsis
+
+Talon is a Python application that will allow a user to perform a more thorough forensic analysis of a Twitter profile by analyzing the content, location, media, and chronological activity of a given username. This tool was developed as a free and open source alternative to tools such as X1 Social Media Discovery and Twitter Investigator.
+
+## Installation
+
+git clone https://github.com/jfk2670/Talon-475_2141-Eddy-Kelleher-Salsone.git
+
+## Usage
+
+python talon.py -­u <username> [-c <count>]
+
+## Motivation
+
+We decided upon this tool for a few reasons. Primarily, we wanted to develop a unique tool that that performed a useful function. Many projects focus on memory/file analysis, recovery, and acquisition. By analyzing Twitter timelines, we accomplish a few different goals, as well. Interacting with the Twitter API gains us valuable programming knowledge, as none of our team members have had much experience using a public API. By doing this, we can become more well­rounded programmers in many ways. 
+ 
+We decided to develop this tool in Python because all members were familiar with this language. With experience in a few different areas of Python development, we decided to use our different strengths to develop a comprehensive and robust tool. 
+
+## Contributors
+
+Jack Kelleher
+Josh Eddy
+Michael Salsone
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2014 ForensicTools
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
--- talon.py
+++ talon.py
@@ -1,5 +1,8 @@
 import sys, os, re
-import HTML
+import html
+from html import HTML
+import logging
+import datetime
 import tweepy
 from tweepy import OAuthHandler
 from optparse import OptionParser
@@ -8,6 +11,20 @@ import codecs
 import zipfile
 import urllib2
 import collections
+
+def setupLogger():
+	"""
+	Sets up Python logger object
+	"""
+	logger = logging.getLogger("TALON")
+	handler = logging.StreamHandler()
+	FORMAT = "%(asctime)-8s [%(levelname)-5s] %(message)s"
+	DATE_FORMAT = "%H:%M:%S"
+	formatter = logging.Formatter(fmt=FORMAT, datefmt=DATE_FORMAT)
+	handler.setFormatter(formatter)
+	logger.addHandler(handler)
+	logger.setLevel(logging.DEBUG)
+	return logger
 
 def setup():
 	"""
@@ -18,23 +35,24 @@ def setup():
 	ACCESS_KEY = '2808956111-uumNUJgW4or9oqXOZzEAwQOgEYbShImKXjZaVkv'
 	ACCESS_SECRET = 'AzHHqljl4IvN20XydnIZnGpUNprjFVYIffXG3kUOfdOaR'
 
-	print "[+] Authenticating..."
+	logger = logging.getLogger("TALON")
+	logger.info("Authenticating...")
 	try:
 		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 		auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 		api = tweepy.API(auth)
-		print "[+] Successfully authenticated!"
+		logger.info("Successfully authenticated!")
 		return tweepy.API(auth)
-		
+
 	except Exception, e:
-		print "[-] Error authenticating to Twitter:"
-		print e
+		logger.error("Error authenticating to Twitter")
+		logger.error(e)
 		exit(0)
 
-def getTimeline(api, username, count=20, verbose=True):
+def getTimeline(api, username, count=20):
 	"""
 	Retrieves timeline of specified user
-	
+
 	Arguments:
 		api			Instance of Twitter API
 		username	Username of target
@@ -42,143 +60,141 @@ def getTimeline(api, username, count=20, verbose=True):
 	Returns:
 		timeline	list of STATUS objects
 	"""
-	if verbose == True:
-		print "[+] Retrieving timeline of @%s..."%username
+	logger = logging.getLogger("TALON")
+	logger.info("Retrieving timeline of @%s...", username)
 	try:
 		timeline = api.user_timeline(screen_name=username, count=count)
-		if verbose == True:
-			print "[+] %s tweet(s) found"%len(timeline)
+		logger.info("%s tweet(s) found", len(timeline))
 		return timeline
-		
+
 	except Exception, e:
-		print "[-] Error retrieving timeline of "+username
-		print e
-		
+		logger.error("Error retrieving timeline of %s", username)
+		logger.error(e)
+
 def printTimeline():
 	"""
 	Prints all statuses from loaded timeline
 	"""
+	logger = logging.getLogger("TALON")
 	try:
 		for status in timeline:
 			printTweet(status)
 		return
 	except Exception, e:
-		print "[-] Error printing timeline"
-		errorLog(e, "printTimeline")
+		logger.error("Error printing timeline")
+		logger.error(e)
 		return
 
 def printTweet(tweet):
 	"""
 	Prints info on statuses from user's timeline
-	
+
 	Arguments:
 		tweet	user status object
 	"""
 	try:
-		print "----"
-		print "%-14s %s" % ("[+] Time:", str(tweet.created_at))
-		print "%-14s %s" % ("[+] Text:", xstr(tweet.text))
-		print "%-14s %s" % ("[+] Location:", geoInfo(tweet))
-		print "%-14s %s" % ("[+] Platform:", xstr(tweet.source))
-		print "%-14s %s" % ("[+] Link:", "http://twitter.com/"+tweet.user.screen_name+"/status/"+str(tweet.id))
+		print "------"
+		print "%-10s %s" % ("Time:", str(tweet.created_at))
+		print "%-10s %s" % ("Text:", xstr(tweet.text))
+		print "%-10s %s" % ("Location:", geoInfo(tweet))
+		print "%-10s %s" % ("Platform:", xstr(tweet.source))
+		print "%-10s %s" % ("Link:", "http://twitter.com/"+tweet.user.screen_name+"/status/"+str(tweet.id))
 		return
 	except Exception, e:
-		print "[-] Error printing tweet"
-		errorLog(e, "printTweet")
+		logger.error("Error printing tweet")
+		logger.error(e)
 		return
-		
+
 def printHelp():
 	"""
 	Prints help menu
-	"""	
-	print "-" * 24
-	print "%-12s %s" % ("Command", "Description")
-	print "-" * 24
-	print "%-12s %s" % ("archive", "Downloads HTML archive of loaded timeline")
-	print "%-12s %s" % ("clear","Clears the terminal")
-	print "%-12s %s" % ("date", "Find tweets in certain date ranges")
-	print "%-12s %s" % ("exit", "Exits the program")
-	print "%-12s %s" % ("help", "Prints this help list")
-	print "%-12s %s" % ("html", "Download timeline to an html file")	
-	print "%-12s %s" % ("live", "Interactive live search for specific queries")
-	print "%-12s %s" % ("match", "Compare timeline against a given wordlist")
-	print "%-12s %s" % ("mentions", "Find most frequently contacted users")
-	print "%-12s %s" % ("new", "Change target account")			
-	print "%-12s %s" % ("user", "Prints basic user info")
-	#print "%-12s %s" % ("advanced", "Prints advanced user info")		
-	print "%-12s %s" % ("zip", "Download and zip timeline")
+	"""
+
 
 def userInfo():
 	"""
 	Prints info on target account
 	"""
+	logger = logging.getLogger("TALON")
 	try:
-		print "%-18s %s" % ("[+] Name:",timeline[0].user.name)
-		print "%-18s %s" % (("[+] Handle:", str(timeline[0].user.screen_name)))
-		print "%-18s %s" % ("[+] About: ", xstr(timeline[0].user.description))
-		print "%-18s %s" % ("[+] Location: ", xstr(timeline[0].user.location))
-		print "%-18s %s" % ("[+] Profile Link:", "http://twitter.com/"+timeline[0].user.screen_name)
-	
+		print "%-8s %s" % ("Name:",timeline[0].user.name)
+		print "%-8s %s" % (("Handle:", str(timeline[0].user.screen_name)))
+		print "%-8s %s" % ("About: ", xstr(timeline[0].user.description))
+		print "%-8s %s" % ("Location: ", xstr(timeline[0].user.location))
+		print "%-8s %s" % ("Profile:", "http://twitter.com/"+timeline[0].user.screen_name)
+
 	except Exception, e:
 		print "[-] Error getting account info"
-		errorLog(e, "userInfo")
+		logger.error(e)
 		return
 
 def changeUser():
 	"""
 	Changes target account
 	"""
+	logger = logging.getLogger("TALON")
 	try:
 		global timeline
 		user = ""
 		user = raw_input("Enter new username: @")
 		count = raw_input("Enter count: ")
 		timeline = getTimeline(api, user, count)
-	
+
 	except Exception, e:
-		print "[-] Unable to change user account, target is still"+options.username
-		errorLog(e, "changeUser")
-		timeline = getTimeline(api, user, count, verbose=False)
+		logger.error("Unable to change user account, target is still %s", options.username)
+		logger.error(e)
+		timeline = getTimeline(api, user, count)
 		return
-		
+
 def dateSearch():
+	logger = logging.getLogger("TALON")
 	try:
 		start_entry = raw_input('Enter start date (YYYY-MM-DD): ')
-		end_entry = raw_input('Enter end data (YYYY-MM-DD): ')
+		end_entry = raw_input('Enter end date (YYYY-MM-DD): ')
 
-		syear, smonth, sday = start_entry.split('-')
-		eyear, emonth, eday = end_entry.split('-')
-		start_date = syear + smonth + sday
-		end_date = eyear + emonth + eday
+		if validateDate(start_entry) and validateDate(end_entry):
+			syear, smonth, sday = start_entry.split('-')
+			eyear, emonth, eday = end_entry.split('-')
+			start_date = syear + smonth + sday
+			end_date = eyear + emonth + eday
+			if start_date > end_date:
+				logger.error("End date must come after start date")
 
-		for status in timeline:
-			date = str(status.created_at)
-			current_date, time = date.split(' ');
-			year, month, day = current_date.split('-', 3)
-			check_date = year + month + day
+			for status in timeline:
+				date = str(status.created_at)
+				current_date, time = date.split(' ');
+				year, month, day = current_date.split('-', 3)
+				check_date = year + month + day
 
-			if check_date > start_date and check_date < end_date:
-				printTweet(status)
-				return
-				
+				if check_date > start_date and check_date < end_date:
+					printTweet(status)
+					return
 	except Exception, e:
-		print "[-] Error performing chronological search"
-		errorLog(e, "dateSearch")
+		logger.error("Error performing chronological search")
+		logger.error(e)
 		return
-	
+
+def validateDate(date_text):
+	logger = logging.getLogger("TALON")
+	try:
+		datetime.strptime(date_text, '%Y-%m-%d')
+		return True
+	except Exception, e:
+		logger.error("One or more dates not in correct format")
+		logger.error(e)
+		return False
+
 def listSearch():
 	"""
 	Will check the currently gathered timeline and match it against the given wordlist
 	"""
+	logger = logging.getLogger("TALON")
 	try:
 		wordlist = raw_input('Enter filename: ')
-		"""
 		if not os.path.isfile(wordlist):
-			print "[-] Error opening file"
-			clear()
-		"""
-		print "[+] Searching timeline for contents of %s..." %wordlist
-		
+			logger.error("%s not found", wordlist)
+			return
+		logger.info("Searching timeline for contents of %s...", wordlist)
 		for status in timeline:
 			wfile = open(wordlist, 'r')
 
@@ -187,17 +203,17 @@ def listSearch():
 				if word.lower() in status.text.lower():
 					printTweet(status)
 		wfile.close()
-					
+
 	except Exception, e:
-		print "[-] Error searching timeline"
-		errorLog(e, "listSearch")
+		logger.error("Error searching timeline")
+		logger.error(e)
 		return
-	
+
 def liveSearch():
 	"""
 	Will check the currently gathered timeline and match it against a specified text query
 	"""
-	
+	logger = logging.getLogger("TALON")
 	try:
 		query = ""
 		while True:
@@ -208,14 +224,15 @@ def liveSearch():
 				if query.lower() in status.text.lower():
 					printTweet(status)
 		return
-		
+
 	except Exception, e:
-		print "[-] Error with search query"
-		errorLog(e, "liveSearch")
+		logger.error("Error with search query")
+		logger.error(e)
 		return
 
 def getImages():
-	print "[+] Downloading images from @%s's timeline..."%str(timeline[0].user.screen_name)
+	logger = logging.getLogger("TALON")
+	logger.info("Downloading images from @%s's timeline...", str(timeline[0].user.screen_name))
 	try:
 		i = 1
 		filename = str(timeline[0].user.screen_name)+"_images.zip"
@@ -226,7 +243,7 @@ def getImages():
 					s = 'image'
 					file = '%s%d' %(s, i)
 					ext = '.jpg'
-					name = file + ext 
+					name = file + ext
 					i += 1
 					img = urllib2.urlopen(media['media_url'])
 					localFile = open(name, 'wb')
@@ -234,53 +251,54 @@ def getImages():
 					localFile.close()
 					zf.write(name)
 					os.remove(name)
-		print "[+] Downloaded %d image(s)"%i
+		logger.info("Downloaded %d image(s)", i)
 		zf.close()
 		return
-		
+
 	except Exception, e:
-		print "[-] Error downloading pictures"
-		errorLog(e, "getImages")
+		logger.error("Error downloading pictures")
+		logger.error(e)
 		return
 
 def archive():
 	"""
 	Downloads HTML archive of currently loaded timeline
 	"""
-	print "[+] Creating HTML archive of timeline..."
-	
+	logger = logging.getLogger("TALON")
+	logger.info("Creating HTML archive of timeline...")
 	try:
-		table = HTML.Table(header_row=['Time', 'Text', 'Location', 'Platform', 'Web Link'])
-		
+		table = HTML.table(header_row=['Time', 'Text', 'Location', 'Platform', 'Web Link'])
+
 		for tweet in timeline:
 			geo =geoInfo(tweet)
 			if geo != "N/A":
 				geo = HTML.link(geo, geoInfo(tweet, URL=True))
 			webUrl = HTML.link('View on web', "http://twitter.com/"+tweet.user.screen_name+"/status/"+str(tweet.id))
-			
+
 			newRow = [str(tweet.created_at), tweet.text.encode('utf-8'), geo, xstr(tweet.source),webUrl]
 			table.rows.append(newRow)
-		
+
 		name = str(timeline[0].user.screen_name)+"_archive_"+datetime.now().strftime('%Y-%m-%d_%H%M')+".html"
 		localFile = open(name, 'w')
 		localFile.writelines(str(table))
 		localFile.close()
-		
+
 		print "[+] HTML archive successfully created"
 		return
-		
+
 	except Exception, e:
 		print "[-] Error creating HTML archive from timeline"
-		errorLog(e, "archive")
-		return							
-		
+		logger.error(e)
+		return
+
 def mentions():
 	"""
 	Prints most common words used in tweets
 	"""
+	logger = logging.getLogger("TALON")
 	mentions = []
-	print "[+] Gathering info on mentions..."
-	
+	logger.info("Gathering info on mentions...")
+
 	try:
 		for tweet in timeline:
 			split = tweet.text.lower().split()
@@ -289,25 +307,25 @@ def mentions():
 					mentions.append(word)
 		counter = collections.Counter(mentions)
 		mentions = counter.most_common()
-		
+
 		print "\nUser                   Qty"
 		print "=========================="
 		try:
 			for i in range(0,10):
 				print '%-20s %05s' % (str(mentions[i][0]), str(mentions[i][1]))
 		except:
-			print "[+] End of mentions"
+			logger.info("End of mentions")
 		return
-	
+
 	except Exception, e:
-		print "[-] Error getting mentions"
-		errorLog(e, "mentions")
+		logger.error("Error getting mentions")
+		logger.error(e)
 		return
-		
+
 def geoInfo(tweet, URL=False):
 	"""
 	Returns short description geo info from tweet
-	
+
 	Arguments:
 		tweet		Tweet object
 		URL			bool, whether or not user wants GMaps URL returned
@@ -322,19 +340,19 @@ def geoInfo(tweet, URL=False):
 				return tweet.place.full_name
 		else:
 			return "N/A"
-	
+
 	except Exception, e:
-		print "[-] Error getting geolocation info"
-		errorLog(e, "printTimeline")
+		logger.error("Error getting geolocation info")
+		logger.error(e)
 		return
-	
+
 def xstr(s):
 	"""
 	Converts null string to 'N/A'
 	"""
 	if s is None:
 		return 'N/A'
-	"""	
+	"""
 	emoji_pattern = r'/[U0001F601-U0001F64F]/u'
 	re.sub(emoji_pattern, '#', s)
 	"""
@@ -350,83 +368,65 @@ def xstr(s):
 		myre = re.compile(u'['
 			u'\U0001F300-\U0001F64F'
 			u'\U0001F680-\U0001F6FF'
-			u'\u2600-\u26FF\u2700-\u27BF]+', 
+			u'\u2600-\u26FF\u2700-\u27BF]+',
 			re.UNICODE)
 	except re.error:
 		# Narrow UCS-2 build
 		myre = re.compile(u'('
 			u'\ud83c[\udf00-\udfff]|'
 			u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
-			u'[\u2600-\u26FF\u2700-\u27BF])+', 
+			u'[\u2600-\u26FF\u2700-\u27BF])+',
 			re.UNICODE)
 	#re.sub(highpoints, '', s)
 	"""
-	return s.encode('utf-8')	
-	
+	return s.encode('utf-8')
+
 def clear():
 	os.system('cls' if os.name == 'nt' else 'clear')
-	
-def errorLog(error, module):
-	"""
-	Writes errors to HTML log file
-	
-	Arguments:
-		error		Error recieved from module
-		module		Function where error was encountered
-	"""
-	try:
-		log = HTML.Table(header_row=['Function', 'Time', 'Info'])
-		
-		entry = [module, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), str(error)]
-		log.rows.append(entry)
-		
-		logFile = open("talon_error_log.html", 'a')
-		logFile.writelines("<br>")
-		logFile.writelines(str(log))
-		logFile.close()
-		
-	except Exception, e:
-		print "ERROR\n"+str(e)
-		return
-		
+
 def main():
+
+	menu =  {'archive':[archive,"Downloads HTML archive of loaded timeline"],
+				'clear':[clear,"Clears the terminal"],
+				'date':[dateSearch,"Find tweets in certain date ranges"],
+				'exit':[exit,"Exits the program"],
+				'help':[printHelp,"Prints this help list"],
+				'list':[listSearch,"Compare timeline against a given wordlist"],
+				'live':[liveSearch,"Interactive live search for specific queries"],
+				'media':[getImages,"Download ZIP archive of images posted by user"],
+				'mentions':[mentions,"Find most frequently contacted users"],
+				'new':[changeUser,"Change target account"],
+				'print':[printTimeline,"Prints timeline"],
+				'user':[userInfo,"Prints basic user info"],
+				#'advanced':advanced, "Prints advanced user info"]
+				}
+
+	logger = setupLogger()
 
 	parser = OptionParser(usage="usage: %prog -u <username>", version="%prog 1.0")
 	parser.add_option("-u", "--username", dest="username", help="Twitter username of target account")
 	parser.add_option("-c", "--count", dest="count", help="Number of tweets to retrieve (default of 20)")
 	(options, args) = parser.parse_args()
-	
+
 	if not options.username:
-		parser.print_help()
+		parser.print_help(menu)
 		exit(0)
-	
-	methodIndex =  {'archive':archive,
-					'clear':clear,
-					'date':dateSearch,
-					'exit':exit,
-					'help':printHelp,
-					'list':listSearch,
-					'live':liveSearch,
-					'media':getImages,
-					'mentions':mentions,
-					'new':changeUser,
-					'print':printTimeline,
-					'user':userInfo
-					}
-	
-	global api, timeline	
+
+	global api, timeline
 	api = setup()
 	timeline = getTimeline(api, options.username, options.count)
-	
+
 	while True:
 		command = raw_input(">>> ")
-		
-		if command in methodIndex.keys():		
-			methodIndex[command]()
+		if command == 'help':
+			print "-" * 24
+			for function in menu.keys():
+				print "%-10s %s" % (function, menu[function][1])
+			print "-" * 24
+		elif command in menu.keys():
+			menu[command][0]()
 		else:
 			print "Invalid command"
-	
+
 if __name__ == "__main__":
 	main()
-
-	
